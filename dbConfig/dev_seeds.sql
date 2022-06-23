ec0aadd0cca7ec6622612332f70a63bea616ff51
Aleopaa <aleksandraopacic@hotmail.co.uk>
2022-06-23 09:30:51 +0100

    first com

---
+++ dbConfig/dev_seeds.sql
@@ -0,0 +1,13 @@
+INSERT INTO posts (title, name, body) 
+VALUES
+(
+    'Hey this is my attempt at the challenge',
+    'Aleks Opacic',
+    'I have a strong suspicion that this will not work'
+),
+(
+     'Hey this is my attempt at the challenge',
+    'Aleks Opacic',
+    'I have a strong suspicion that this will not work'
+
+);
---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+
+const server = express();
+server.use(cors());
+server.use(express.json());
+
+const postRoutes = require('./routes/post')
+server.use('/post', postRoutes)
+
+server.get('/', (req, res) => res.send('Welcome to the coding challenge server'))
+
+module.exports = server
